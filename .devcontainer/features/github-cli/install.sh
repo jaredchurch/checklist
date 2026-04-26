02dd571e341aeb21ffa9e9ec2c8e4b78b33b725f
Jared Church <pjaredchurch@gmail.com>
2026-04-26 19:40:08 +1200

    test if causing error on build

--- .devcontainer/features/github-cli/install.sh
+++ .devcontainer/features/github-cli/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -e
+# set -e
 
-echo -e "\n🐙 Installing GitHub CLI..."
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/github-cli-archive.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt-get update
-sudo apt-get install gh
-echo "✅ Done"
+# echo -e "\n🐙 Installing GitHub CLI..."
+# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/github-cli-archive.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install gh
+# echo "✅ Done"
 
 ### End of File
