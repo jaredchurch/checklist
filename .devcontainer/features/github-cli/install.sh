f5b2e577a42fb4fcef8a41cd61e4852a57bc6e98
Jared Church <pjaredchurch@gmail.com>
2026-04-26 19:46:03 +1200

    based on github docs

--- .devcontainer/features/github-cli/install.sh
+++ .devcontainer/features/github-cli/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-# set -e
+set -e
 
-# echo -e "\n🐙 Installing GitHub CLI..."
-# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/github-cli-archive.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-# sudo apt-get update
-# sudo apt-get install gh
-# echo "✅ Done"
+# refer: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian
+echo -e "\n🐙 Installing GitHub CLI..."
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt-get update \
+	&& sudo apt-get install gh -y
+echo "✅ Done"
 
 ### End of File
